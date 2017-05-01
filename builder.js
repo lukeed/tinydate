3fe53dd86bd12b03ec66cd1b3743d5c1c1dba06c
Luke Edwards <luke.edwards05@gmail.com>
2017-05-01 15:22:04 -0700

    add gzip size logging

--- builder.js
+++ builder.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const { resolve } = require('path');
 const { minify } = require('uglify-js');
+const pretty = require('pretty-bytes');
+const sizer = require('gzip-size');
 const pkg = require('./package');
 
 const dist = resolve('dist');
@@ -13,11 +15,17 @@ if (!fs.existsSync(dist)) {
 fs.readFile(entry, (_, buf) => {
 	// copy as `es` module
 	fs.writeFile(resolve(pkg.module), buf);
+
 	// transform to `cjs` module
 	const data = buf.toString().replace('export default', 'module.exports =');
 	fs.writeFile(resolve(pkg.main), data);
+
 	// produce minified output
 	const { code } = minify(data, { fromString:true });
 	const file = resolve(pkg.main.replace('.js', '.min.js'));
 	fs.writeFile(file, code);
+
+	// output gzip size
+	const int = sizer.sync(code);
+	console.log(`> gzip size: ${ pretty(int) }`);
 });
